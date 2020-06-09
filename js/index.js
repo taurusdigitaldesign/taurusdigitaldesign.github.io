43bdb2bcade79efd963bcc7e4065cc56a7c0c231
王勤政 <qinzheng_wang@ziguangcn.com>
2020-06-09 18:10:57 +0800

    fix: change cdn

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
 $("#js-rotating").Morphext({
-    // The [in] animation type. Refer to Animate.css for a list of available animations.
-    animation: "animate__animated animate__flipInX",
-    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
-    separator: ",",
-    // The delay between the changing of each phrase in milliseconds.
-    speed: 2000,
-    complete: function () {
-        // Called after the entrance animation is executed.
-    }
+  // The [in] animation type. Refer to Animate.css for a list of available animations.
+  animation: "flipInX",
+  // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+  separator: ",",
+  // The delay between the changing of each phrase in milliseconds.
+  speed: 2000,
+  complete: function () {
+    // Called after the entrance animation is executed.
+  },
 });
+
+// var wow = new WOW({
+//   boxClass: "wow",
+//   animateClass: "animated",
+//   offset: 0,
+//   mobile: true,
+//   live: true,
+// });
+// wow.init();
 
 new WOW().init();
